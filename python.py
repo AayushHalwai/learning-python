3f5dbf1e717e3108983160b86fe7e9a26590e0ff
Aayush Halwai <90670094+AayushHalwai@users.noreply.github.com>
2025-09-24 21:20:15 +0545

    1st day

---
+++ python.py
@@ -0,0 +1,41 @@
+'''print("Hello, World!")
+#strings
+food = "rice"
+print(f"I like {food}")
+#integers
+age = 21
+print(f"I am {age} years old")
+#floats
+height = 5.11
+print(f"My height is {height} feet ")
+#booleans
+man = True
+print(f"Are you a man? {man}")
+if man:
+    print("You are a man")
+else:
+    print("You are not a man")
+#multiple assignment
+x, y, z = "Orange", "Banana", "Cherry"
+print(f"Fruits: {x}, {y}, {z}")
+#typecasting
+name = "aayush"
+gpa = 9.8
+age = 21
+is_graduated = False
+gpa = str(gpa)
+print(gpa)
+print(type(gpa))
+'''
+'''#user input
+name = input("Enter your name: ")
+print(f"Hello, {name}!")
+age = int(input("Enter your age: "))
+print(f"You are {age} years old")
+height = float(input("Enter your height in feet: "))
+print(f"You are {height} feet tall")'''
+#exercise 1 area of rectangle
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+area = length * width
+print(f"The area of the rectangle is {area} square units")
