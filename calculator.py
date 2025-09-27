5b7c3b9240de5e4d0c46160eb686a97aee3a9a79
halwaiaayush@gmail.com <90670094+AayushHalwai@users.noreply.github.com>
2025-09-27 17:26:15 +0545

    used if elif to create calculator

---
+++ calculator.py
@@ -0,0 +1,15 @@
+#calculator using if-elif-else
+num1 = float(input("Enter Number 1: "))
+num2 = float(input("Enter Number 2: "))
+operator = input("Enter operator (+, -, *, /): ")
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+    result = num1 / num2
+else:
+    result = "Invalid operator"
+print(f"Result: {round(result, 2)}")
